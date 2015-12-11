66290b3dfda80201a5cf2f63beac44cc00f4f9c8
Jonathon Reinhart <jreinhart@cc-sw.com>
2015-12-11 18:44:44 -0500

    setup.py: initial commit
    Installs both docker_registry module, and docker-registry-console
    script.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+    name = 'py-docker-registry',
+    version = '0.0.1',
+    install_requires = [
+        'requests',
+    ],
+
+    py_modules = ['docker_registry'],
+    scripts = ['docker-registry-console'],
+
+    # Metadata
+    author = 'Jonathon Reinhart',
+    author_email = 'Jonathon.Reinhart@gmail.com',
+    description = 'Python client for Docker Registry API',
+    keywords = 'docker registry',
+)
